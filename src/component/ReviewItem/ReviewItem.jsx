32fab2d840e3ce33e6950727a698affa331c2418
Ferdousnayeem <mostafanayeem11@gmail.com>
2023-07-18 09:49:23 +0600

    code added

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-const ReviewItem = () => {
+const ReviewItem = ({product}) => {
+
+    const {id, img, price, name, quantity} = product;
+
     return (
-        <div>
-            <h3>Review Items</h3>
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className="review-details">
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <FontAwesomeIcon className='delete-icon' icon={faTrashCan}/>
+            </button>
         </div>
     );
 };
